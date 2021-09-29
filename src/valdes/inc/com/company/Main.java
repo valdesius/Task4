be851fd9333b35b7083f3a15f3de4712bd81eafc
valdesius <v.inc@inbox.ru>
2021-09-29 10:15:27 +0300

    V2.01

--- src/valdes/inc/com/company/Main.java
+++ src/valdes/inc/com/company/Main.java
@@ -3,70 +3,77 @@ package valdes.inc.com.company;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int num = inputNumber();
-        checkNum(num);
+        int a = readNumber();
+// calculateEvenAndOddSumInNumber(a);
+        int b = readNumber();
+        int c = readNumber();
+        calculateFromAToB(a, b , c);
     }
 
-    private static int inputNumber(){
+    private static int readNumber() {
         int number = 0;
+        Scanner scanner = new Scanner(System.in);
         try {
-            System.out.print("Введите число с одинаковым количеством четных и нечетных цифр:  ");
+            System.out.print("Введите число с одинаковым количеством четных и нечетных цифр: ");
             number = scanner.nextInt();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.print("Неверно введенные данные ");
             System.exit(1);
         }
-
         return number;
 
     }
-    private static void checkNum(int num){
-        int n1 = num;
-        int n2 = num;
-        int n3 = num;
-        int n4 = num;
-        // проверка для двухзначных чисел
-        if (num>9 && num<100) {
-            int firstDigit = n1 / 10;
-            int secondDigit = n2 % 10;
-            System.out.println("");
 
-            if ((firstDigit % 2 == 0 && secondDigit % 2 != 0) || (firstDigit % 2 != 0 && secondDigit % 2 == 0)){
-                System.out.println("проходит");
-
-            } else {
-                System.out.println("цифры не подходят по усл. ");
-            }
-
+    private static void calculateEvenAndOddSumInNumber(int number) {
+        int a = number;
+        int even = 0;
+        int odd = 0;
+        for (int i = 1; a / i > 0; i *= 10) {
+            if ((a / i) % 2 == 0)
+                even++;
+            else
+                odd++;
         }
-        // проверка для четырехзначных
-        if (num > 999 && num < 100000){
-            // 2631
-            int firstDigit = n1 / 1000;
-            System.out.println(firstDigit);
+        System.out.println("Четных чисел: " + even + " Нечетных чисел: " + odd);
 
-            int secondDigit = n2 / 100 % 10;
-            System.out.println(secondDigit);
-
-            int thirdDigit = n3 % 100 / 10;
-            System.out.println(thirdDigit);
-
-            int fourDigit = n4 % 10;
-            System.out.println(fourDigit);
-
-            if (((firstDigit % 2 == 0 && secondDigit % 2 != 0) || (firstDigit % 2 != 0 && secondDigit % 2 == 0)) ||  ((thirdDigit % 2 == 0
-                    && fourDigit % 2 != 0) ||  (thirdDigit % 2 != 0 && fourDigit % 2 == 0)) || ((firstDigit % 2 != 0 && secondDigit %2 != 0 &&
-                    thirdDigit % 2 == 0 && fourDigit % 2 == 0) || (firstDigit % 2 == 0 && secondDigit % 2 == 0 && thirdDigit %2 != 0 && fourDigit %2 != 0))
-            ){
-                System.out.println("проходит");
-            } else {
-                System.out.println("цифры не подходят по усл. ");
-            }
+        if (even == odd) {
+            System.out.println("Число походит под условие задачи!");
+        } else {
+            System.out.println("Число не походит под условие задачи...");
         }
-
     }
+
+    private static int calculateFromAToB(int numberA, int numberB, int number){
+
+        int sum = 0;
+        for(int m = numberA; m <= numberA; m++){
+            int a = number;
+            int even = 0;
+            int odd = 0;
+            for (int i = 1; a / i > 0; i *= 10) {
+                if ((a / i) % 2 == 0)
+                    even++;
+                else
+                    odd++;
+            }
+            System.out.println("Четных чисел: " + even + " Нечетных чисел: " + odd);
+
+            if (even == odd) {
+                System.out.println("Число походит под условие задачи!");
+            } else {
+                System.out.println("Число не походит под условие задачи...");
+            }
+
+
+            sum += m;
+
+        }
+
+
+        System.out.print(sum);
+        return sum;
+    }
+
 }
