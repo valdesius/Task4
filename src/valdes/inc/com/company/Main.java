b3cb54fa3df0c4cabab567bb8c1e2184f219bbe8
valdesius <v.inc@inbox.ru>
2021-10-01 21:57:36 +0300

    V2.03

--- src/valdes/inc/com/company/Main.java
+++ src/valdes/inc/com/company/Main.java
@@ -7,15 +7,15 @@ public class Main {
     public static void main(String[] args) {
         int a = readNumber();
         int b = readNumber();
-
+        isNumberFitsConditions();
         int sum = calculateFromAToB(a, b);
-        printSumFromAToB(sum);
-
+        System.out.println("Sum of numbers: " + sum);
     }
 
     private static int readNumber() {
         int number = 0;
         Scanner scanner = new Scanner(System.in);
+
         try {
             System.out.print("Введите число с одинаковым количеством четных и нечетных цифр: ");
             number = scanner.nextInt();
@@ -23,12 +23,33 @@ public class Main {
             System.out.print("Неверно введенные данные ");
             System.exit(1);
         }
+
         return number;
     }
 
-    private static int calculateFromAToB(int numberA, int numberB) {
+    private static void isNumberFitsConditions() {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int even = 0;
+        int odd = 0;
 
+        for (int i = 1; m / i > 0; i *= 10) {
+            if ((m / i) % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+
+        if (even == odd) {
+            System.out.println("True number");
+        } else {
+            System.out.println("False number");
+        }
+    }
+
+    private static int calculateFromAToB(int numberA, int numberB) {
         int sum = 0;
+
         for (int m = numberA; m <= numberB; m++) {
             int even = 0;
             int odd = 0;
@@ -46,13 +67,8 @@ public class Main {
                 System.out.println("Пропуск числа");
             }
         }
-
         return sum;
     }
 
-
-    private static void printSumFromAToB(int sum) {
-        System.out.println("сумма чисел от А до B: " + sum);
-    }
 
 }
